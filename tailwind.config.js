f14e087e1d71860105cdc2128f70da28db9be972
unknown <stp40@case.edu>
2023-11-27 15:16:57 -0500

    product specifications completed + overarching rules and styles applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(circle, rgba(81,183,221,1) 7%, rgba(0,91,174,1) 100%)',
+      },
+      width: {
+        'custom': '63%',
+      },
+      colors: {
+        'custom-blue': 'rgb(81,183,221)',
+        'accent-blue': '#3673aa',
+        'hover-blue': '#204566'
+      }
+    },
   },
   plugins: [],
 }
